dc38edf35760de74bb81ac26e2d449a9f831a1b1
Gabriel Hernandez <ghernandez1305@gmail.com>
2021-02-03 01:07:06 -0400

    Add project files.

---
+++ LabelGenerator/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using LabelGenerator.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using EasyPost;
+EasyPost.ClientManager.SetCurrent("EZTK7e150bc466114ebfb2df0bb9b04facdc9psA5T79tUvOaksDVeYOnw");
+
+
+namespace LabelGenerator.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+
+
+
+
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
